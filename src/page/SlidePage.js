28af1d2f4bbfec66220ed48c6b756f59de0fe0ed
신용주 <lukas.sin@kakaocorp.com>
2019-09-26 15:18:07 +0900

    Slick slide test

--- src/page/SlidePage.js
+++ src/page/SlidePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import SlideTemplate from './template/SlideTemplate'
+import SlickSlide from '../components/organisms/SlickSlide'
 
 const SlidePage = () => {
   return (
-    <SlideTemplate></SlideTemplate>
+    <SlideTemplate
+      slide ={<SlickSlide></SlickSlide>} 
+    ></SlideTemplate>
   );
 };
 
